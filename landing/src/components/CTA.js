c425731de25d9a37aad15c64e73f4877124976f0
Vorapon Kachapornkul <hakao97@gmail.com>
2024-03-15 21:33:27 +0800

    Finish content

--- landing/src/components/CTA.js
+++ landing/src/components/CTA.js
@@ -1,35 +1,28 @@
 export default function Calltoaction() {
-    return (
-      <div className="bg-white">
-        <div className="mx-auto max-w-7xl px-6 py-16 lg:px-8">
-          <div className="overflow-hidden rounded-lg bg-green-700 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
-            <div className="px-6 pb-12 pt-10 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:px-15 xl:py-20">
-              <div className="lg:self-center">
-                <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                  <span className="block">Create your impact</span>
-                  <span className="block">Reserve your credits today</span>
-                </h2>
-                <p className="mt-4 text-lg leading-6 text-green-200">
-                  Ac euismod vel sit maecenas id pellentesque eu sed consectetur. Malesuada adipiscing sagittis vel nulla
-                  nec.
-                </p>
-                <a
-                  href="#"
-                  className="mt-8 inline-flex items-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-green-600 shadow hover:bg-green-50"
-                >
-                  Inquire and Reserve Now
-                </a>
-              </div>
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-16 lg:px-8">
+        <div className="overflow-hidden rounded-lg bg-green-700 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
+          <div className="px-6 pb-12 pt-10 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:px-15 xl:py-20">
+            <div className="lg:self-center">
+              <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                <span className="block">Create your impact</span>
+                <span className="block">Reserve your credits today</span>
+              </h2>
+              <p className="mt-4 text-lg leading-6 text-green-200">
+                Ready to make a measurable difference?
+                <p>Contact UtilityHarvest to explore how our carbon credits can align with your sustainability objectives.</p>
+              </p>
+              <a href="#" className="mt-8 inline-flex items-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-green-600 shadow hover:bg-green-50">
+                Inquire and Reserve Now
+              </a>
             </div>
-            <div className="aspect-h-3 aspect-w-5 -mt-6 md:aspect-h-1 md:aspect-w-2">
-              <img
-                className="translate-x-6 translate-y-6 transform rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
-                src="https://tailwindui.com/img/component-images/full-width-with-sidebar.jpg"
-                alt="App screenshot"
-              />
-            </div>
+          </div>
+          <div className="aspect-h-3 aspect-w-5 -mt-6 md:aspect-h-1 md:aspect-w-2">
+            <img className="translate-x-6 translate-y-6 transform rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20" src="https://tailwindui.com/img/component-images/full-width-with-sidebar.jpg" alt="App screenshot" />
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  );
+}
--- landing/src/components/Content.js
+++ landing/src/components/Content.js
@@ -1,4 +1,4 @@
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from "@heroicons/react/20/solid";
 
 export default function Content() {
   return (
@@ -6,11 +6,7 @@ export default function Content() {
       <div className="mx-auto max-w-7xl lg:flex lg:justify-between lg:px-8 xl:justify-end">
         <div className="lg:flex lg:w-1/2 lg:shrink lg:grow-0 xl:absolute xl:inset-y-0 xl:right-1/2 xl:w-1/2">
           <div className="relative h-80 lg:-ml-8 lg:h-auto lg:w-full lg:grow xl:ml-0">
-            <img
-              className="absolute inset-0 h-full w-full bg-gray-50 object-cover"
-              src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80"
-              alt=""
-            />
+            <img className="absolute inset-0 h-full w-full bg-gray-50 object-cover" src="https://images.unsplash.com/photo-1559136555-9303baea8ebd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-x=.4&w=2560&h=3413&&q=80" alt="" />
           </div>
         </div>
         <div className="px-6 lg:contents">
@@ -18,40 +14,30 @@ export default function Content() {
             <p className="text-base font-semibold leading-7 text-green-600">Utalizing untapped value</p>
             <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Why Utility Harvest?</h1>
             <div className="mt-10 max-w-xl text-base leading-7 text-gray-700 lg:max-w-none">
-              <p>
-                Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
-                vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque
-                erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-                semper sed amet vitae sed turpis id.
-              </p>
+              <p>At UtilityHarvest, our dedication to precision in carbon offsetting sets us apart. Through meticulous verification processes and strategic partnerships with Southeast Asian farmers, we guarantee reliable carbon credits that deliver tangible environmental benefits.</p>
               <ul role="list" className="mt-8 space-y-8 text-gray-600">
                 <li className="flex gap-x-3">
                   <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
                   <span>
-                    <strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
-                    consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                    blanditiis ratione.
+                    <strong className="font-semibold text-gray-900">Precision.</strong> We use meticulously verified carbon credits through DMRV technologies to ensure the most tracable and transparent credit of carbon offsetting.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
                   <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
                   <span>
-                    <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
-                    ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                    <strong className="font-semibold text-gray-900">Reliability.</strong> Consistent quality and impact. We deliver benefits throughout all harvesting seasons and conditions through our diverse portfolio of projects.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
                   <ServerIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
                   <span>
-                    <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                    vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+                    <strong className="font-semibold text-gray-900">Sustainability.</strong> Support for eco-friendly farming practices. We work with local farmers to ensure that our projects are also sustainable and beneficial to the local community.
                   </span>
                 </li>
                 <li className="flex gap-x-3">
                   <ServerIcon className="mt-1 h-5 w-5 flex-none text-green-600" aria-hidden="true" />
                   <span>
-                    <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                    vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
+                    <strong className="font-semibold text-gray-900">Responsibility.</strong> Direct contribution to your corporate ESG goals. We provide a direct and transparent way to contribute to your corporate ESG goals.
                   </span>
                 </li>
               </ul>
@@ -60,5 +46,5 @@ export default function Content() {
         </div>
       </div>
     </div>
-  )
+  );
 }
